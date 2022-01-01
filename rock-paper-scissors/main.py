74f05065775535343501cb17437b203f8f96982c
vishesh-bajpayee <vishesh.bajpayee@gmail.com>
2022-01-01 09:50:30 +0530

    added rock-paper-scissorts game

--- rock-paper-scissors/main.py
+++ rock-paper-scissors/main.py
@@ -1,6 +1,7 @@
 import random 
 # ROCK
 rock = """
+	ROCK
     _______
 ---'   ____)
       (_____)
@@ -10,6 +11,7 @@ rock = """
 """
 # PAPER
 paper = """
+	PAPER
      _______
 ---'    ____)____
            ______)
@@ -19,6 +21,7 @@ paper = """
 """
 # SCISSORS 
 scissors = """
+	SCISSORS
     _______
 ---'   ____)____
           ______)
@@ -28,5 +31,28 @@ scissors = """
 """
 
 choices_list = [rock,paper,scissors]
+random_generator = random.randint(0,2)
+random_choice = choices_list[random_generator]
+
+user_choice_num = int(input("Enter 1 for rock, 2 for paper, 3 scissors: ")) -1
+user_choice = choices_list[user_choice_num]
+
+print(f" YOU CHOSE:\n{user_choice}")
+print(f"COMPUTER CHOSE:\n{random_choice}")
+
+print(random_generator)
+print(user_choice_num)
+# Conditions 
+
+# Draw Condition
+if random_generator == user_choice_num:
+	print("Its a Draw")
+elif random_generator == 0 and user_choice_num == 2:
+	print("Computer Wins")
+elif random_generator == 1 and user_choice_num == 0:
+	print('Computer Wins')
+elif random_generator == 2 and user_choice_num == 1:
+	print("Computer Wins")
+
 
 
